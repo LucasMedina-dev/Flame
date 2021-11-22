4d3234486a4d7d19a6c0c892975704c50833fd7b
LucasMedina-dev <medinalucasian@gmail.com>
2021-11-22 20:21:42 -0300

    Desafio Clase 3

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../styles/css/style.css';
+import Header from './Header';
+
+
+const App = ()=> {
+    return (
+        <Header/>
+    )
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header_title">
+                <a href="#" className="header_title-style">Noose</a>
+            </div>
+            <div className="header_nav">
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Productos</a></li>
+                    <li><a href="#">Contacto</a></li>
+                    <li><a href="#">Carrito</a></li>
+                </ul>
+            </div>
+        </header>
+    )
+}
+
+export default Header
