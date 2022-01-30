6ecb1d4d34e48f343cc4123621cae47aa0122ebf
LucasMedina-dev <medinalucasian@gmail.com>
2022-01-30 19:36:25 -0300

    Se arregló el panel administrador

--- src/components/Account/AdminAccountOptions/AccountFormContainer.js
+++ src/components/Account/AdminAccountOptions/AccountFormContainer.js
@@ -3,20 +3,27 @@ import ModifyItem from './Forms/ModifyItem';
 import DeleteItem from './Forms/DeleteItem';
 import ModifyOffer from './Forms/ModifyOffer';
 import { useParams } from 'react-router-dom';
-
+import { useContext } from 'react';
+import { userContext } from '../../Contexts/AccountContext';
 const AccountFormContainer = () => {
     const {option}=useParams()
-
-    if(option==="CreateItem"){
+    const {status}=useContext(userContext)
+    let redirect
+    if (!status){
+        return <></>
+    }else{
+        redirect=option
+    }
+    if(redirect==="CreateItem"){
         return(<CreateItem/>)
     }
     if(option==="ModifyItem"){
         return(<ModifyItem/>)
     }
-    if(option==="DeleteItem"){
+    if(redirect==="DeleteItem"){
         return(<DeleteItem/>)
     }
-    if(option==="ModifyOffer"){
+    if(redirect==="ModifyOffer"){
         return(<ModifyOffer/>)
     }
 };
