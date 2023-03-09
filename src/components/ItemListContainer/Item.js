fd126ccdd79953a122df1d4db5674277979a8027
LucasMedina-dev <medinalucasian@gmail.com>
2023-03-09 18:22:43 -0300

    se reemplaza img estandar en caso de campo vacio

--- src/components/ItemListContainer/Item.js
+++ src/components/ItemListContainer/Item.js
@@ -1,6 +1,10 @@
 import {Link} from 'react-router-dom'
 
 const Item = (props) => {
+    let img=props.img;
+    if(!props.img){
+        img="https://upload.wikimedia.org/wikipedia/commons/8/8f/Example_image.svg";
+    }
     return (
         <div className="product_item">
             <div className="product_description">
